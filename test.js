e9bc70c90d00f702fc45c99a013a19abddb2b485
Ahmad Jiha <ahmad.j.jiha@gmail.com>
2022-07-06 09:45:46 -0700

    Make .eslintrc.json file

---
+++ test.js
@@ -0,0 +1,17 @@
+
+
+function test() {
+  return 15;
+}
+
+
+const variable = true ? true : false;
+
+const nextVar = true ? true : false;
+
+const nextNext = true ? true: false;
+
+const a = 'test single' + 'b';
+
+const obj = {ame: 'value'};
+
