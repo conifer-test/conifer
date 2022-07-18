10ccfc4e18caffe8ee08b3c89a0d6b29f2129aa6
Ainaa Azman <ainaasakinah@gmail.com>
2022-07-19 01:29:09 +0900

    resolve dependencies

--- src/utils/create-test-run-results-bash.js
+++ src/utils/create-test-run-results-bash.js
@@ -1,6 +1,7 @@
 const { execSync } = require('child_process');
 
 const generateFinalReport = () => {
+  console.log('Generating Parallel Test Results...');
   execSync('npx mochawesome-merge ./coniferJSONReports/*.json -o ./coniferJSONReports/output.json');
   execSync('npx mochawesome-report-generator marge ./coniferJSONReports/output.json');
 };
