6da586a454e6b644df77f518755b35c229e3ac5b
Lawrence Tam <lawrenceatam@gmail.com>
2022-07-20 14:56:25 -0700

    Fix teardown

--- src/utils/coniferTeardown.js
+++ src/utils/coniferTeardown.js
@@ -1,10 +1,12 @@
 const Promisify = require('./promisify');
 const ora = require('ora');
 const spinner = ora();
+const { CONIFER_LOCAL_DIRECTORY } = require('./coniferConfig');
 
 const teardown = async () => {
   spinner.start('Tearing down conifer AWS infrastructure');
-  await Promisify.spawner(['cdk', ['destroy', '-f', '*']]);
+  process.chdir(`${CONIFER_LOCAL_DIRECTORY}/deploy`);
+  await Promisify.spawner('cdk', ['destroy', '-f']);
   await Promisify.execute(
     'aws ecr delete-repository --repository-name conifer-test --force'
   );
