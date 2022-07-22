2140cc5751c512fb9547c0d46f949df27a9e4142
Ahmad Jiha <ahmad.j.jiha@gmail.com>
2022-07-22 12:06:16 -0700

    Add conifer dockerfile dependency

--- src/utils/coniferDockerfile.js
+++ src/utils/coniferDockerfile.js
@@ -14,7 +14,7 @@ FROM cypress/included:10.3.0
 
 INCLUDE+ Dockerfile
 
-RUN ${packageManager} add wait-on dotenv @aws-sdk/client-s3 @aws-sdk/client-dynamodb @aws-sdk/util-dynamodb mochawesome
+RUN ${packageManager} add wait-on dotenv @aws-sdk/client-s3 @aws-sdk/client-dynamodb @aws-sdk/util-dynamodb @aws-sdk/lib-dynamodb mochawesome
 
 ADD ./conifer-start.sh /
 RUN chmod +x /conifer-start.sh
