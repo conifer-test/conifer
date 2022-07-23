1399229c93ae29b2004f6c2eaf039ee1163b4ba2
Ahmad Jiha <ahmad.j.jiha@gmail.com>
2022-07-23 12:33:57 -0700

    Fix minor dependency bug and increment version

--- src/utils/delete-test-run-S3.js
+++ src/utils/delete-test-run-S3.js
@@ -6,9 +6,9 @@ const {
   CONIFER_CONFIG_FILE,
 } = require('./coniferConfig');
 const CDK_OUTPUTS_PATH = `${CONIFER_LOCAL_DIRECTORY}/cdk_outputs.json`;
-const cdkOutputs = JSON.parse(fs.readFileSync(CDK_OUTPUTS_PATH));
 
 const deleteLastRunS3 = () => {
+  const cdkOutputs = JSON.parse(fs.readFileSync(CDK_OUTPUTS_PATH));
   const bucketName = cdkOutputs.ConiferCdkStack.bucketName;
   console.log('Deleting last test run files in S3');
   execSync(`aws s3 rm s3://${bucketName}/ --recursive `)
--- src/utils/download-test-run-S3.js
+++ src/utils/download-test-run-S3.js
@@ -6,10 +6,10 @@ const {
   CONIFER_CONFIG_FILE,
 } = require('./coniferConfig');
 const CDK_OUTPUTS_PATH = `${CONIFER_LOCAL_DIRECTORY}/cdk_outputs.json`;
-const cdkOutputs = JSON.parse(fs.readFileSync(CDK_OUTPUTS_PATH));
 
 
 const downloadLastRunS3 = () => {
+  const cdkOutputs = JSON.parse(fs.readFileSync(CDK_OUTPUTS_PATH));
   const bucketName = cdkOutputs.ConiferCdkStack.bucketName;
   console.log('Downloading all reports in the last run');
   execSync(`aws s3 sync s3://${bucketName} ./coniferJSONReports`);
