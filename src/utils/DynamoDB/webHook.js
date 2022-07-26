128efd71e07a414163ec065af3921c591be35171
Lawrence Tam <lawrenceatam@gmail.com>
2022-07-25 17:03:39 -0700

    Fix ddb-client region and testRunID

---
+++ src/utils/DynamoDB/webHook.js
@@ -0,0 +1,12 @@
+const axios = require('axios');
+
+async function sendWebhooks(data) {
+  const webhookURL = 'http://localhost:5001/testFileUpdated';
+  try {
+    await axios.post(webhookURL, data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = sendWebhooks;
