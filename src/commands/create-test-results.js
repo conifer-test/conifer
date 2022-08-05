55990c0960b49cf8aefd81ac29dcc612b8f58ab3
Ahmad Jiha <ahmad.j.jiha@gmail.com>
2022-08-05 11:45:22 -0700

    Fix bug: await async function
    Co-authored-by: Lawrence Tam <lawrenceatam@gmail.com>
    Co-authored-by: Ahmad Jiha <ahmad.j.jiha@gmail.com>

--- src/commands/create-test-results.js
+++ src/commands/create-test-results.js
@@ -4,13 +4,13 @@ const deleteTestRunS3 = require('../utils/delete-test-run-S3');
 const downloadTestRunS3 = require('../utils/download-test-run-S3');
 const createTestRunResults = require('../utils/create-test-run-result-script');
 
-const generateTestResults = () => {
+const generateTestResults = async () => {
   console.log('Wait and receive info that test is completed');
   console.log(`${chalk.cyan('Generating your parallel test results now...')}`);
 
-  downloadTestRunS3();
+  await downloadTestRunS3();
   // deleteTestRunS3();
-  createTestRunResults();
+  await createTestRunResults();
 };
 
 module.exports = {
--- src/utils/download-test-run-S3.js
+++ src/utils/download-test-run-S3.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
-const { execSync } = require('child_process');
 const {
   CONIFER_LOCAL_DIRECTORY,
 } = require('./coniferConfig');
 const CDK_OUTPUTS_PATH = `${CONIFER_LOCAL_DIRECTORY}/cdk_outputs.json`;
 const ora = require('ora');
 const spinner = ora({ color: 'green' });
+const { execute } = require('./promisify');
 
 
 const downloadLastRunS3 = async () => {
@@ -13,7 +13,7 @@ const downloadLastRunS3 = async () => {
   const bucketName = cdkOutputs.ConiferCdkStack.bucketName;
 
   spinner.start('Downloading all reports in the last run');
-  execSync(`aws s3 sync s3://${bucketName} ./coniferJSONReports`);
+  await execute(`aws s3 sync s3://${bucketName} ./coniferJSONReports`);
   spinner.succeed('Results downloaded successfully');
 };
 
